65f289f1b699bca9a6e43fa4f60f3aca372ee35e
saveligulas <saveli.gulas@gmail.com>
2023-03-11 22:07:47 +0100

    initial Commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/errorHandler/handler/ApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package gulas.saveli.StadtLandFluss.errorHandler.handler;
+
+import gulas.saveli.finalLibrary.library.errorHandler.exception.ApiException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value={ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ApiException exception = new ApiException(
+                e.getMessage(),
+                e,
+                badRequest,
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(exception,badRequest);
+    }
+}
