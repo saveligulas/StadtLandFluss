45a001768e0609ce95c48ae4d5913cf2471105b6
saveligulas <saveli.gulas@gmail.com>
2023-03-11 13:00:36 +0100

    Initial commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/StadtLandFlussApplication.java
@@ -0,0 +1,13 @@
+package gulas.saveli.StadtLandFluss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadtLandFlussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StadtLandFlussApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gulas/saveli/StadtLandFluss/StadtLandFlussApplicationTests.java
@@ -0,0 +1,13 @@
+package gulas.saveli.StadtLandFluss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadtLandFlussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
