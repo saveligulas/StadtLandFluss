877ecccff8908a6ef9515fc4752cd85fb770d8b8
saveligulas <saveli.gulas@gmail.com>
2023-03-11 22:02:29 +0100

    initial Commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/config/GameDataConfiguration.java
@@ -0,0 +1,12 @@
+package gulas.saveli.StadtLandFluss.game.logic.config;
+
+import gulas.saveli.StadtLandFluss.game.logic.GameData;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GameDataConfiguration {
+
+    @
+    public GameData gameData = new GameData();
+}
