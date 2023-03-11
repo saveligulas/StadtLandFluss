8bb852f187766fbe39ae03c1afaf56e24fbea25d
saveligulas <saveli.gulas@gmail.com>
2023-03-11 22:01:03 +0100

    removed unused authorities

--- src/main/java/gulas/saveli/StadtLandFluss/user/Authority.java
+++ src/main/java/gulas/saveli/StadtLandFluss/user/Authority.java
@@ -1,5 +1,5 @@
 package gulas.saveli.StadtLandFluss.user;
 
 public enum Authority {
-    USER_AUTHORITIES, SEE_USERS, EDIT_LIBRARY, EDIT_USERS, DELETE_BOOK, DELETE_USER;
+    USER_AUTHORITIES, ADMIN_AUTHORITIES;
 }
