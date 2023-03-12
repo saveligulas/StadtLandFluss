e833bf6b5384dbd93ad8bd2ce1afa6bfb3163070
saveligulas <saveli.gulas@gmail.com>
2023-03-12 14:33:22 +0100

    initial Commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/security/logger/UserLogRepository.java
@@ -0,0 +1,45 @@
+package gulas.saveli.StadtLandFluss.security.logger;
+
+import gulas.saveli.StadtLandFluss.repo.ObjectRepository;
+import gulas.saveli.StadtLandFluss.repo.UserRepository;
+import gulas.saveli.StadtLandFluss.user.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UserLogRepository implements ObjectRepository<User> {
+    private Map<String, User> repository;
+
+    public UserLogRepository() {
+        this.repository = new HashMap<>();
+    }
+
+
+    @Override
+    public void store(String token, User user) {
+        this.repository.put(token, user);
+    }
+
+    @Override
+    public User retrieve(String token) {
+        return repository.get(token);
+    }
+
+    @Override
+    public User search(String name) {
+        for(User user:repository.entrySet().toArray(new User[repository.size()])) {
+            if(user.getFirstName().equals(name) || user.getLastName().equals(name)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public User delete(String token) {
+        return null;
+    }
+}
