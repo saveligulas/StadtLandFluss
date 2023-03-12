af15e78aaca0800d9fc40ea1e256cbc571e4cc31
saveligulas <saveli.gulas@gmail.com>
2023-03-12 22:22:16 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/security/logger/UserService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/logger/UserService.java
@@ -1,7 +1,21 @@
 package gulas.saveli.StadtLandFluss.security.logger;
 
+import gulas.saveli.StadtLandFluss.repo.UserRepository;
+import gulas.saveli.StadtLandFluss.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void setToken(long id, String token) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User with id " + id + " does not exist"));
+        user.setToken(token);
+    }
 }
