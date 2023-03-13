2a9af5b1b97ae8a476fd2b85fefffb2012321f2b
saveligulas <saveli.gulas@gmail.com>
2023-03-13 16:01:55 +0100

    switched method return type

--- src/main/java/gulas/saveli/StadtLandFluss/repo/ObjectRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/ObjectRepository.java
@@ -6,7 +6,7 @@ public interface ObjectRepository<T> {
 
     public T retrieve(String token);
 
-    public T search(String name);
+    public boolean search(String name);
 
     public T delete(String token);
 }
