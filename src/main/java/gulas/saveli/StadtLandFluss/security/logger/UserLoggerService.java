288cbade4bc4eed921dff3a38b81220b727d4397
saveligulas <saveli.gulas@gmail.com>
2023-03-13 16:02:29 +0100

    code revamp. Changed User mapping to String

--- src/main/java/gulas/saveli/StadtLandFluss/security/logger/UserLoggerService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/logger/UserLoggerService.java
@@ -1,6 +1,7 @@
 package gulas.saveli.StadtLandFluss.security.logger;
 
 import gulas.saveli.StadtLandFluss.errorHandler.handler.ApiRequestException;
+import gulas.saveli.StadtLandFluss.repo.UserRepository;
 import gulas.saveli.StadtLandFluss.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,35 +16,33 @@ public class UserLoggerService {
 
     @Autowired
     private final UserLogRepository userLogRepository;
+    private final UserRepository userRepository;
 
-    public void save(String token, User user) {
+    public void connect(String token, String email) {
         List<String> tokens = userLogRepository.retrieveTokens();
         if(tokens.contains(token)) {
             throw new ApiRequestException("token already exists");
         }
-        List<User> users = userLogRepository.retrieveUsers();
-        if(users.contains(user)) {
+        List<String> users = userLogRepository.retrieveUsers();
+        if(users.contains(email)) {
             throw new ApiRequestException("user already exists");
         }
-        userLogRepository.store(token, user);
+        userLogRepository.store(token, email);
     }
 
-    public void delete(String token) {
+    public void disconnect(String token) {
         if(!userLogRepository.retrieveTokens().contains(token)) {
-            for(User user : userLogRepository.retrieveUsers()) {
-                if(user.getFullName().equals(token)) {
-                    userLogRepository.deleteByName(token);
-                    return;
-                }
-            }
             throw new ApiRequestException("token not found");
         }
         userLogRepository.delete(token);
     }
 
     public List<String> getConnectedUsersNames() {
+        List<String> emails = userLogRepository.retrieveUsers();
         List<String> names = new ArrayList<>();
-        for(User user:userLogRepository.retrieveUsers()) {
+        for(String email : emails) {
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new ApiRequestException("user not found"));
             names.add(user.getFullName());
         }
         return names;
