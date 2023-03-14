109d0dce693d5514f02b302ab48c9bb4daad5655
saveligulas <saveli.gulas@gmail.com>
2023-03-14 23:10:57 +0100

    removed data annotation

--- src/main/java/gulas/saveli/StadtLandFluss/security/auth/disconnect/InvalidToken.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/auth/disconnect/InvalidToken.java
@@ -3,17 +3,34 @@ package gulas.saveli.StadtLandFluss.security.auth.disconnect;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Table(name = "DISCONNECTED_TOKENS")
 public class InvalidToken {
     @Id
     private Long id;
     private String token;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        InvalidToken that = (InvalidToken) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
