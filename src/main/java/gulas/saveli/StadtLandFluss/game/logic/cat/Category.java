f425d36abd0bc3354b1dffb44b048e202b7c27bd
saveligulas <saveli.gulas@gmail.com>
2023-03-14 23:10:17 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/cat/Category.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/cat/Category.java
@@ -1,4 +1,35 @@
 package gulas.saveli.StadtLandFluss.game.logic.cat;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Boolean hasOfficialChecker;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Category category = (Category) o;
+        return getId() != null && Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
