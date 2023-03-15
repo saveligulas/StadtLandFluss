4d8aee19d87f92c345ccb0a3cb84fe99ba2ca5ba
saveligulas <saveli.gulas@gmail.com>
2023-03-15 23:14:33 +0100

    import change

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/Game.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/Game.java
@@ -1,5 +1,6 @@
 package gulas.saveli.StadtLandFluss.game.logic;
 
+import gulas.saveli.StadtLandFluss.game.logic.model.GameData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
