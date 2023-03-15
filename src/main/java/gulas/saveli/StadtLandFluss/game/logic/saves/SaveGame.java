d70b67698d379a020a2d44a417321a909ffe69d3
saveligulas <saveli.gulas@gmail.com>
2023-03-15 23:12:52 +0100

    added maps

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/saves/SaveGame.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/saves/SaveGame.java
@@ -1,15 +1,40 @@
 package gulas.saveli.StadtLandFluss.game.logic.saves;
 
-import gulas.saveli.StadtLandFluss.game.logic.GameData;
+import gulas.saveli.StadtLandFluss.game.logic.model.Answer;
+import gulas.saveli.StadtLandFluss.game.logic.model.GameData;
+import gulas.saveli.StadtLandFluss.user.User;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class SaveGame extends GameData {
-    private Map<String, List<String>> userInputs;
+@Entity
+public class SaveGame {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToMany
+    @JoinTable(
+            name = "user_save_games",
+            joinColumns = @JoinColumn(name = "save_game_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users;
+    @ElementCollection
+    @CollectionTable(name = "user_answers", joinColumns = @JoinColumn(name = "save_game_id"))
+    @MapKeyJoinColumn(name = "user_id")
+    private Map<User, List<Answer>> userAnswers;
+    @ElementCollection
+    @CollectionTable(name = "save_game_scores", joinColumns = @JoinColumn(name = "save_game_id"))
+    @MapKeyJoinColumn(name = "user_id")
+    @Column(name = "value")
+    private Map<User, Integer> scores;
+    @Embedded
+    private GameData gameData;
 }
