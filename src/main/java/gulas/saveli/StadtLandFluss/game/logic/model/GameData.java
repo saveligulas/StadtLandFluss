ce061cb93331deafbde0caf776e12c954d3e55c0
saveligulas <saveli.gulas@gmail.com>
2023-03-15 23:14:19 +0100

    moved and made embedded

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/GameData.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/GameData.java
@@ -1,18 +1,21 @@
-package gulas.saveli.StadtLandFluss.game.logic;
+package gulas.saveli.StadtLandFluss.game.logic.model;
 
 import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
-import gulas.saveli.StadtLandFluss.user.User;
+import gulas.saveli.StadtLandFluss.game.logic.model.Round;
+import jakarta.persistence.Embeddable;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Embeddable
 public class GameData {
 
-    private List<User> players;
+    private List<String> usernames;
     private Integer rounds;
     private Integer currentRound;
     private List<Category> categories;
     private List<Character> characters;
+    private List<Round> roundsData;
 
 }
