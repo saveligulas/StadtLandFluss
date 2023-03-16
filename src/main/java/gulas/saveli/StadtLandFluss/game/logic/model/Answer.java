8cc583d548f065e32e67960f09bb33098d5577c7
saveligulas <saveli.gulas@gmail.com>
2023-03-16 23:36:03 +0100

    added embeddable

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/Answer.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/Answer.java
@@ -1,9 +1,11 @@
 package gulas.saveli.StadtLandFluss.game.logic.model;
 
 import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
+import jakarta.persistence.Embeddable;
 import lombok.Data;
 
 @Data
+@Embeddable
 public class Answer {
     private Category category;
     private String answer;
