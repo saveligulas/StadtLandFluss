0cbaf950c416f21a381a7bb7bc9d844583f5446e
saveligulas <saveli.gulas@gmail.com>
2023-03-16 13:20:03 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/game/controller/GameController.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/controller/GameController.java
@@ -1,4 +1,40 @@
 package gulas.saveli.StadtLandFluss.game.controller;
 
+import gulas.saveli.StadtLandFluss.builder.ThymeleafModelAndViewBuilder;
+import gulas.saveli.StadtLandFluss.game.logic.GameDataService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/game")
+@RequiredArgsConstructor
 public class GameController {
+
+    @Autowired
+    private final ThymeleafModelAndViewBuilder thymeleafModelAndViewBuilder;
+    @Autowired
+    private final GameDataService gameDataService;
+
+    @GetMapping
+    public ModelAndView viewGame() {
+        return thymeleafModelAndViewBuilder.build("game");
+    }
+
+    @GetMapping("/table/categories")
+    @ResponseBody
+    @CrossOrigin
+    public List<String> getCategoriesOfCurrentGame() {
+        return gameDataService.getCategories();
+    }
+
+    @GetMapping("/table/character")
+    @ResponseBody
+    @CrossOrigin
+    public Character getCurrentCharacter() {
+        return gameDataService.getCurrentCharacter();
+    }
 }
