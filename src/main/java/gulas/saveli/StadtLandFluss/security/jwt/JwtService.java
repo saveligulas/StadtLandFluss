03b71b7bd1d3c065098934378cf390d8c6835a1a
saveligulas <saveli.gulas@gmail.com>
2023-03-16 13:20:59 +0100

    changed expiration date to 1 day

--- src/main/java/gulas/saveli/StadtLandFluss/security/jwt/JwtService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/jwt/JwtService.java
@@ -41,7 +41,7 @@ public class JwtService {
                 .setClaims(extractClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5 ))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 ))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
