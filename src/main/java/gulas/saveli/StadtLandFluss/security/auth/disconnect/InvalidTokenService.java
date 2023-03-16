da04990efb68f00d3de5e44d673c8f3df7717064
saveligulas <saveli.gulas@gmail.com>
2023-03-16 13:20:17 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/security/auth/disconnect/InvalidTokenService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/auth/disconnect/InvalidTokenService.java
@@ -1,4 +1,18 @@
 package gulas.saveli.StadtLandFluss.security.auth.disconnect;
 
+import gulas.saveli.StadtLandFluss.repo.InvalidTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class InvalidTokenService {
+
+    @Autowired
+    private final InvalidTokenRepository invalidTokenRepository;
+
+    public void save(String token) {
+        //TODO
+    }
 }
