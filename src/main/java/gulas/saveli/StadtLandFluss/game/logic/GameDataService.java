de69fea98f323b52575c765baeb805cccf29d544
saveligulas <saveli.gulas@gmail.com>
2023-03-16 13:21:25 +0100

    added methods

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/GameDataService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/GameDataService.java
@@ -1,8 +1,13 @@
 package gulas.saveli.StadtLandFluss.game.logic;
 
+import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
+import gulas.saveli.StadtLandFluss.game.logic.model.GameData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +16,17 @@ public class GameDataService {
     @Autowired
     private final GameData gameData;
 
+
+    public Character getCurrentCharacter() {
+        int currentRound = gameData.getCurrentRound();
+        return gameData.getCharacters().get(currentRound);
+    }
+
+    public List<String> getCategories() {
+        List<String> categoryNames = new ArrayList<>();
+        for(Category category : gameData.getCategories()) {
+            categoryNames.add(category.getName());
+        }
+        return categoryNames;
+    }
 }
