7ab6d0026625451066f70c58e2728a8728c9c3cc
saveligulas <saveli.gulas@gmail.com>
2023-03-17 13:58:31 +0100

    added wires

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/ApplicationConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/ApplicationConfig.java
@@ -1,12 +1,19 @@
 package gulas.saveli.StadtLandFluss.security.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import gulas.saveli.StadtLandFluss.repo.CategoryRepository;
 import gulas.saveli.StadtLandFluss.repo.InvalidTokenRepository;
 import gulas.saveli.StadtLandFluss.repo.UserRepository;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,6 +22,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -33,15 +42,17 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    @Autowired
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("user with email " + username + " does not exist"));
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
+    @Autowired
+    public AuthenticationProvider authenticationProvider(UserRepository userRepository) {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setUserDetailsService(userDetailsService(userRepository));
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
