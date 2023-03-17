7047cbc4c7291f6505ae25bfc8f4ff0fbb82bba8
saveligulas <saveli.gulas@gmail.com>
2023-03-17 13:58:46 +0100

    made entity

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/GameData.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/GameData.java
@@ -3,19 +3,45 @@ package gulas.saveli.StadtLandFluss.game.logic.model;
 import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
 import gulas.saveli.StadtLandFluss.game.logic.model.Round;
 import jakarta.persistence.Embeddable;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
 
 import java.util.List;
 
-@Data
-@Embeddable
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GameData {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private List<String> usernames;
     private Integer rounds;
     private Integer currentRound;
     private List<Category> categories;
     private List<Character> characters;
     private List<Round> roundsData;
+
+    public GameData(List<String> usernames, Integer rounds, Integer currentRound, List<Category> categories, List<Character> characters, List<Round> roundsData) {
+        this.usernames = usernames;
+        this.rounds = rounds;
+        this.currentRound = currentRound;
+        this.categories = categories;
+        this.characters = characters;
+        this.roundsData = roundsData;
+    }
+
+    public void advanceRound() {
+        this.currentRound++;
+    }
+
+    public void addRoundData(Round round) {
+        this.roundsData.add(round);
+    }
 
 }
