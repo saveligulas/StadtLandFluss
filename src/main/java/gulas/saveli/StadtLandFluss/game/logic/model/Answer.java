7b68dba21bd3bfeeee181981b01cf68898f75da4
saveligulas <saveli.gulas@gmail.com>
2023-03-17 13:58:07 +0100

    switched category to string

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/Answer.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/Answer.java
@@ -2,11 +2,14 @@ package gulas.saveli.StadtLandFluss.game.logic.model;
 
 import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
 @Embeddable
 public class Answer {
-    private Category category;
+
+    private String categoryName;
     private String answer;
+
 }
