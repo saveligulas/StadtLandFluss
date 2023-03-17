6471444f04b1edb0cd1c93a58c5374ca897c566d
saveligulas <saveli.gulas@gmail.com>
2023-03-17 15:39:30 +0100

    made Service class Transactional and added methods

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/GameDataService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/GameDataService.java
@@ -3,9 +3,13 @@ package gulas.saveli.StadtLandFluss.game.logic;
 import gulas.saveli.StadtLandFluss.errorHandler.handler.ApiRequestException;
 import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
 import gulas.saveli.StadtLandFluss.game.logic.model.GameData;
+import gulas.saveli.StadtLandFluss.repo.CategoryRepository;
+import gulas.saveli.StadtLandFluss.repo.GameDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +17,15 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class GameDataService {
 
     @Autowired
-    private final GameData gameData;
+    private final GameDataRepository gameDataRepository;
 
-
-    public void setCharactersAndRounds(List<Character> characters) {
+    public void setCharactersAndRounds(Long gameDataId, List<Character> characters) {
+        GameData gameData = gameDataRepository.findById(gameDataId)
+                .orElseThrow(() -> new ApiRequestException("could not find game with id " + gameDataId));
         String characterString =  characters.stream().map(Object::toString).collect(Collectors.joining());
         if(!characterString.matches("[a-zA-Z]+")) {
             throw new ApiRequestException("Invalid characters contained in list");
@@ -41,12 +47,22 @@ public class GameDataService {
         gameData.setCharacters(characters);
     }
 
-    public Character getCurrentCharacter() {
+    public Character getCurrentCharacter(Long gameDataId) {
+        GameData gameData = gameDataRepository.findById(gameDataId)
+                .orElseThrow(() -> new ApiRequestException("could not find game with id " + gameDataId));
         int currentRound = gameData.getCurrentRound();
         return gameData.getCharacters().get(currentRound);
     }
 
-    public List<String> getCategories() {
+    public List<Character> getAllCharacters(Long gameDataId) {
+        GameData gameData = gameDataRepository.findById(gameDataId)
+                .orElseThrow(() -> new ApiRequestException("could not find game with id " + gameDataId));
+        return gameData.getCharacters();
+    }
+
+    public List<String> getCategories(Long gameDataId) {
+        GameData gameData = gameDataRepository.findById(gameDataId)
+                .orElseThrow(() -> new ApiRequestException("could not find game with id " + gameDataId));
         List<String> categoryNames = new ArrayList<>();
         for(Category category : gameData.getCategories()) {
             categoryNames.add(category.getName());
