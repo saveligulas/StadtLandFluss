e46af09348b8ff445cce63813f5fec00cb22ca39
saveligulas <saveli.gulas@gmail.com>
2023-03-18 20:53:15 +0100

    initial Commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/AnswerList.java
@@ -0,0 +1,24 @@
+package gulas.saveli.StadtLandFluss.game.logic.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "answer_maplist")
+public class AnswerList {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ElementCollection
+    @OrderColumn
+    private List<Answer> list;
+}
