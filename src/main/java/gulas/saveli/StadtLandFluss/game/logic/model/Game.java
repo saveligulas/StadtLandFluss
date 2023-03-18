71944bfa92076805b1fa1e22ce276fc6eac586e5
saveligulas <saveli.gulas@gmail.com>
2023-03-18 20:53:34 +0100

    initial Commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/Game.java
@@ -0,0 +1,42 @@
+package gulas.saveli.StadtLandFluss.game.logic.model;
+
+import gulas.saveli.StadtLandFluss.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @OneToMany(targetEntity = User.class)
+    private List<User> players;
+    private String hostUsername;
+    private Integer rounds;
+    @OrderColumn
+    private List<Character> characters = new ArrayList<>();
+    @ElementCollection
+    @OrderColumn
+    private List<Category> categories = new ArrayList<>();
+    @ElementCollection
+    private Map<String, AnswerList> usernameAnswerMap = new HashMap<>();
+
+    public Game(String hostUsername, Integer rounds, List<Character> characters, List<Category> categories) {
+        this.hostUsername = hostUsername;
+        this.rounds = rounds;
+        this.characters = characters;
+        this.categories = categories;
+    }
+}
