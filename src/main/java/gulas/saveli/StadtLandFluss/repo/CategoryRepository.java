d626be376a7d110bbfa880f397cd3c8a76c25fde
saveligulas <saveli.gulas@gmail.com>
2023-03-18 10:17:09 +0100

    added query

--- src/main/java/gulas/saveli/StadtLandFluss/repo/CategoryRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/CategoryRepository.java
@@ -2,8 +2,13 @@ package gulas.saveli.StadtLandFluss.repo;
 
 import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("SELECT c FROM Category c WHERE c.name = ?1")
+    public Optional<Category> findByName(String name);
 }
