29f0e9d0b33b49c237739f6130ea2f024097a5d9
saveligulas <saveli.gulas@gmail.com>
2023-03-18 10:17:32 +0100

    added mappings for game

--- src/main/java/gulas/saveli/StadtLandFluss/game/controller/GameController.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/controller/GameController.java
@@ -2,6 +2,7 @@ package gulas.saveli.StadtLandFluss.game.controller;
 
 import gulas.saveli.StadtLandFluss.builder.ThymeleafModelAndViewBuilder;
 import gulas.saveli.StadtLandFluss.game.logic.GameDataService;
+import gulas.saveli.StadtLandFluss.game.logic.cat.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -24,17 +25,24 @@ public class GameController {
         return thymeleafModelAndViewBuilder.build("game");
     }
 
-    @GetMapping("/table/categories")
+    @PostMapping ("/table/{gameId}/category")
     @ResponseBody
     @CrossOrigin
-    public List<String> getCategoriesOfCurrentGame() {
-        return gameDataService.getCategories();
+    public List<Category> setCategories(@PathVariable("gameId") Long gameId, List<String> categories) {
+        return gameDataService.setCategories(gameId, categories);
     }
 
-    @GetMapping("/table/character")
+    @GetMapping("/table/{gameId}/category")
     @ResponseBody
     @CrossOrigin
-    public Character getCurrentCharacter() {
-        return gameDataService.getCurrentCharacter();
+    public List<String> getCategoriesOfCurrentGame(@PathVariable("gameId") Long gameId) {
+        return gameDataService.getCategories(gameId);
+    }
+
+    @GetMapping("/table/{gameId}/character")
+    @ResponseBody
+    @CrossOrigin
+    public Character getCurrentCharacter(@PathVariable("gameId") Long gameId){
+        return gameDataService.getCurrentCharacter(gameId);
     }
 }
