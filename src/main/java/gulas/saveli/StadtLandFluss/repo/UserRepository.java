f1e95d1d94f5edc3f0f5c19a74b51d30f3fe58db
saveligulas <saveli.gulas@gmail.com>
2023-03-19 14:58:16 +0100

    fixed query

--- src/main/java/gulas/saveli/StadtLandFluss/repo/UserRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/UserRepository.java
@@ -3,6 +3,7 @@ package gulas.saveli.StadtLandFluss.repo;
 
 import gulas.saveli.StadtLandFluss.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findByEmail(String email);
 
 }
