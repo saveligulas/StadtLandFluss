bdaad830a4a617b33903da0c81beee8d1296acd7
saveligulas <saveli.gulas@gmail.com>
2023-03-19 14:58:28 +0100

    initial Commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/resp/GameResponse.java
@@ -0,0 +1,12 @@
+package gulas.saveli.StadtLandFluss.game.logic.model.resp;
+
+import lombok.Data;
+
+@Data
+public class GameResponse {
+
+    private Long id;
+    private String hostUsername;
+    private Integer playerCount;
+
+}
