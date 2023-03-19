e28631c46b78959d9ae06a46e13c25bcb09cac37
saveligulas <saveli.gulas@gmail.com>
2023-03-19 14:56:59 +0100

    code cleanup and added method with only one parameter

--- src/main/java/gulas/saveli/StadtLandFluss/game/service/GameService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/service/GameService.java
@@ -3,6 +3,7 @@ package gulas.saveli.StadtLandFluss.game.service;
 import gulas.saveli.StadtLandFluss.errorHandler.handler.ApiRequestException;
 import gulas.saveli.StadtLandFluss.game.logic.model.Category;
 import gulas.saveli.StadtLandFluss.game.logic.model.Game;
+import gulas.saveli.StadtLandFluss.game.logic.model.resp.GameResponse;
 import gulas.saveli.StadtLandFluss.repo.CategoryRepository;
 import gulas.saveli.StadtLandFluss.repo.GameRepository;
 import gulas.saveli.StadtLandFluss.repo.UserRepository;
@@ -17,8 +18,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.springframework.util.ClassUtils.isPresent;
-
 @Service
 @RequiredArgsConstructor
 public class GameService {
@@ -27,30 +26,93 @@ public class GameService {
     private final GameRepository gameRepository;
     @Autowired
     private final CategoryRepository categoryRepository;
+    @Autowired
     private final UserRepository userRepository;
+    @Autowired
+    private final RandomUtils randomUtils;
+
+    public Game hostGame(String username) {
+        List<Category> categoryList = new ArrayList<>();
+        List<String> categoryNames = new ArrayList<>() {{
+            add("Stadt");
+            add("Land");
+            add("Fluss");
+        }};
+        for(String str : categoryNames) {
+            Optional<Category> categoryOptional = categoryRepository.findByName(str);
+            Category category;
+            if(categoryOptional.isEmpty()) {
+                category = new Category(str, false);
+                categoryRepository.save(category);
+            } else {
+                category = categoryOptional.get();
+            }
+            categoryList.add(category);
+        }
+        Optional<Game> optionalGame = gameRepository.findByHostUsername(username);
+        if(optionalGame.isPresent()) {
+            throw new ApiRequestException("User with email " + username + " has already hosted a game");
+        }
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+        if(optionalUser.isEmpty()) {
+            System.out.println("could not find user");
+            throw new ApiRequestException("User with email " + username + " does not exist");
+        }
+        if(optionalUser.get().isPlaying()) {
+            System.out.println("User in game");
+            throw new ApiRequestException("User is already in a game");
+        }
+        Integer rounds = 3;
+        List<Character> characters = randomUtils.getRandomCharacters(3);
+        Game game = new Game(username, rounds, characters, categoryList);
+        game.setPlayers(new ArrayList<>(Arrays.asList(optionalUser.get())));
+        gameRepository.save(game);
+        return game;
+    }
 
     @Transactional
     public void hostGame(String username, Integer rounds, List<Character> characters, List<String> categoryNames) {
         List<Category> categoryList = new ArrayList<>();
         for(String str : categoryNames) {
             Optional<Category> categoryOptional = categoryRepository.findByName(str);
+            Category category;
             if(categoryOptional.isEmpty()) {
-                Category category = new Category(str, false);
+                category = new Category(str, false);
                 categoryRepository.save(category);
-                categoryList.add(category);
             } else {
-                Category category = categoryOptional.get();
-                categoryList.add(category);
+                category = categoryOptional.get();
             }
+            categoryList.add(category);
         }
-        Optional<User> optionalUser = gameRepository.findByUsername(username);
-        if(optionalUser.isPresent()) {
-            throw new ApiRequestException("User with username " + optionalUser.get().getEmail() + " has already hosted a game");
+        Optional<Game> optionalGame = gameRepository.findByHostUsername(username);
+        if(optionalGame.isPresent()) {
+            throw new ApiRequestException("User with username " + username + " has already hosted a game");
         }
-        optionalUser = userRepository.findByEmail(username); //TODO
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+        if(optionalUser.isEmpty()) {
+            throw new ApiRequestException("User with email " + username + " does not exist");
+        }
+        if(optionalUser.get().getIsInGame()) {
+            throw new ApiRequestException("User is already in a game");
+        }
         Game game = new Game(username, rounds, characters, categoryList);
-        game.setPlayers(new ArrayList<>(Arrays.asList()));
+        game.setPlayers(new ArrayList<>(Arrays.asList(optionalUser.get())));
         gameRepository.save(game);
+    }
 
+    public List<GameResponse> getGamesResponseList() {
+        List<Game> games = gameRepository.findAll();
+        if(games.size() == 0) {
+            return null;
+        }
+        List<GameResponse> gameResponses = new ArrayList<>();
+        GameResponse response = new GameResponse();
+        for(Game game : games) {
+            response.setId(game.getId());
+            response.setHostUsername(game.getHostUsername());
+            response.setPlayerCount(game.getPlayerCount());
+            gameResponses.add(response);
+        }
+        return gameResponses;
     }
 }
