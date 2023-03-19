bbdec1c7a0ec71f6191104c981c98d6d8834584b
saveligulas <saveli.gulas@gmail.com>
2023-03-19 14:55:16 +0100

    added mappings to get and host games

--- src/main/java/gulas/saveli/StadtLandFluss/game/controller/AuthHomeController.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/controller/AuthHomeController.java
@@ -1,11 +1,15 @@
 package gulas.saveli.StadtLandFluss.game.controller;
 
 import gulas.saveli.StadtLandFluss.builder.ThymeleafModelAndViewBuilder;
+import gulas.saveli.StadtLandFluss.game.logic.model.Game;
+import gulas.saveli.StadtLandFluss.game.logic.model.resp.GameResponse;
+import gulas.saveli.StadtLandFluss.game.service.GameService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/home")
@@ -13,10 +17,26 @@ import org.springframework.web.servlet.ModelAndView;
 public class AuthHomeController {
 
     private final ThymeleafModelAndViewBuilder thymeleafModelAndViewBuilder;
+    @Autowired
+    private final GameService gameService;
 
     @GetMapping
     public ModelAndView view() {
         return thymeleafModelAndViewBuilder.build("home");
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    @CrossOrigin
+    public List<GameResponse> getGamesResponseList() {
+        return gameService.getGamesResponseList();
+    }
+
+    @PostMapping("/host")
+    @ResponseBody
+    @CrossOrigin
+    public Game hostGame(@RequestHeader("HostUsername") String host) {
+        return gameService.hostGame(host);
     }
 
 }
