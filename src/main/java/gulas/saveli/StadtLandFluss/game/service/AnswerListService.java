8ea622e12496210ce284b2f4f6e6ec7be55b9006
saveligulas <saveli.gulas@gmail.com>
2023-03-20 21:01:05 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/game/service/AnswerListService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/service/AnswerListService.java
@@ -1,4 +1,16 @@
 package gulas.saveli.StadtLandFluss.game.service;
 
+import gulas.saveli.StadtLandFluss.repo.AnswerListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AnswerListService {
+
+    @Autowired
+    private final AnswerListRepository answerListRepository;
+
+
 }
