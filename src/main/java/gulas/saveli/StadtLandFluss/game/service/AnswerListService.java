c813d1918d9f0b6634ddd57632e782333a738b3e
saveligulas <saveli.gulas@gmail.com>
2023-03-20 21:16:11 +0100

    added method

--- src/main/java/gulas/saveli/StadtLandFluss/game/service/AnswerListService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/service/AnswerListService.java
@@ -1,9 +1,14 @@
 package gulas.saveli.StadtLandFluss.game.service;
 
+import gulas.saveli.StadtLandFluss.game.logic.model.AnswerList;
+import gulas.saveli.StadtLandFluss.game.logic.model.Category;
 import gulas.saveli.StadtLandFluss.repo.AnswerListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +17,13 @@ public class AnswerListService {
     @Autowired
     private final AnswerListRepository answerListRepository;
 
-
+    public List<AnswerList> setupEmptyAnswerLists(int amount, List<Category> categories) {
+        List<AnswerList> answerLists = new ArrayList<>();
+        for(int i = 0; i < amount; i++) {
+            AnswerList answerList = new AnswerList(categories);
+            answerListRepository.save(answerList);
+            answerLists.add(answerList);
+        }
+        return answerLists;
+    }
 }
