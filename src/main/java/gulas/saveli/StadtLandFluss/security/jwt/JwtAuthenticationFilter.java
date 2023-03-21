08f41c781af222faf214e2e6131aff180d55b3f3
saveligulas <saveli.gulas@gmail.com>
2023-03-21 20:47:17 +0100

    added filter to check if sent email/username equals the jwt email

--- src/main/java/gulas/saveli/StadtLandFluss/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/jwt/JwtAuthenticationFilter.java
@@ -36,14 +36,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String jwt;
         final String userEmail;
         final String authHeader = request.getHeader("Authorization");
+        final String emailHeader = request.getHeader("Username");
 
-        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if(authHeader == null || !authHeader.startsWith("Bearer ") || emailHeader == null) {
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if(!userEmail.equals(emailHeader)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        if(SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
             if(jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
