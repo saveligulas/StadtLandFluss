bb03198969207f21740994bf75702a069bb3e675
saveligulas <saveli.gulas@gmail.com>
2023-03-23 19:26:47 +0100

    initial Commit

---
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/resp/GameInfoResponse.java
@@ -0,0 +1,11 @@
+package gulas.saveli.StadtLandFluss.game.logic.model.resp;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Data
+@ResponseBody
+public class GameInfoResponse {
+
+
+}
