f2015b2317f26672d547c7c56340988076199c3b
saveligulas <saveli.gulas@gmail.com>
2023-03-23 19:30:27 +0100

    added mapping for game info

--- src/main/java/gulas/saveli/StadtLandFluss/game/controller/LobbyController.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/controller/LobbyController.java
@@ -2,6 +2,7 @@ package gulas.saveli.StadtLandFluss.game.controller;
 
 import gulas.saveli.StadtLandFluss.builder.ThymeleafModelAndViewBuilder;
 
+import gulas.saveli.StadtLandFluss.game.logic.model.resp.GameInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,5 +26,12 @@ public class LobbyController {
     @CrossOrigin
     public void disconnectUser(@RequestHeader("Authorization") String token) {
         token = token.replace("Bearer ", "");
+    }
+
+    @GetMapping("/{gameId}")
+    @ResponseBody
+    @CrossOrigin
+    public GameInfoResponse viewGameInfo(@PathVariable("gameId") String gameId) {
+        return null;
     }
 }
