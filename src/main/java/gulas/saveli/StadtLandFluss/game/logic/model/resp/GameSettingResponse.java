f7298f16eda95d0769eabfef00322df70278c835
saveligulas <saveli.gulas@gmail.com>
2023-03-25 12:54:18 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/resp/GameSettingResponse.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/resp/GameSettingResponse.java
@@ -1,4 +1,12 @@
 package gulas.saveli.StadtLandFluss.game.logic.model.resp;
 
+import lombok.Data;
+
+@Data
 public class GameSettingResponse {
+
+    private String roundsMessage;
+    private String characterMessage;
+    private String categoryMessage;
+
 }
