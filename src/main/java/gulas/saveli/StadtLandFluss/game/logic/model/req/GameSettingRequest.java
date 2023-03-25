dc7a5e6cb175f242760f5279a40a644618b66295
saveligulas <saveli.gulas@gmail.com>
2023-03-25 12:25:51 +0100

    added variables

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/req/GameSettingRequest.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/req/GameSettingRequest.java
@@ -1,7 +1,18 @@
 package gulas.saveli.StadtLandFluss.game.logic.model.req;
 
+import gulas.saveli.StadtLandFluss.game.logic.model.Category;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.OrderColumn;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class GameSettingRequest {
+
+    private Integer rounds;
+    private List<Character> characters = new ArrayList<>();
+    private List<String> categoryNames = new ArrayList<>();
+
 }
