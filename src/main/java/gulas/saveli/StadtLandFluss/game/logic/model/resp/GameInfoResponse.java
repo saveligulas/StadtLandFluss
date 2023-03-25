cf51e33ca6bbfdb1d8e48d60989e0ca247776275
saveligulas <saveli.gulas@gmail.com>
2023-03-25 12:54:24 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/resp/GameInfoResponse.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/model/resp/GameInfoResponse.java
@@ -1,11 +1,19 @@
 package gulas.saveli.StadtLandFluss.game.logic.model.resp;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @ResponseBody
+@Builder
 public class GameInfoResponse {
 
+    private List<String> players = new ArrayList<>();
+    private Integer rounds;
+    private List<String> categoryNames = new ArrayList<>();
 
 }
