26c1221cb90db587f807a0991023dc020bbcfca4
saveligulas <saveli.gulas@gmail.com>
2023-03-25 12:30:03 +0100

    initial Commit

--- src/main/java/gulas/saveli/StadtLandFluss/game/service/CategoryService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/service/CategoryService.java
@@ -1,4 +1,27 @@
 package gulas.saveli.StadtLandFluss.game.service;
 
+import gulas.saveli.StadtLandFluss.game.logic.model.Category;
+import gulas.saveli.StadtLandFluss.repo.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    @Autowired
+    private final CategoryRepository categoryRepository;
+
+    public Category convertCategoryString(String categoryName) {
+        Optional<Category> categoryOptional = categoryRepository.findByName(categoryName);
+        if(categoryOptional.isPresent()) {
+            return categoryOptional.get();
+        }
+        Category category = new Category(categoryName, false);
+        categoryRepository.save(category);
+        return category;
+    }
 }
