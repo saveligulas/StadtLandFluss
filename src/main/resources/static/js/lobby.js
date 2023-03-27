ff5509b36e0bae2a5df764038c79d7c2b36b3c8f
saveligulas <saveli.gulas@gmail.com>
2023-03-27 11:00:12 +0200

    added and removed functions

--- src/main/resources/static/js/lobby.js
+++ src/main/resources/static/js/lobby.js
@@ -2,40 +2,104 @@ import { getCookie } from './cookies.js';
 console.log("loaded");
 const disconnectButton = document.querySelector('#disconnect');
 const jwtToken = getCookie('Authorization');
-const startButton = document.querySelector('#start');
-startButton.disabled = true;
+const username = getCookie('Username');
+const gameId = getCookie('GameID');
 
-function fetchData() {
-    fetch('http://192.168.1.27:8081', {
-        method: 'GET',
-        headers: {
-            'Authorization': jwtToken,
-            'Content-Type': 'application/json'
-        }
+window.addEventListener('DOMContentLoaded', function() {
+  fetch('/api/show-add-category')
+    .then(function(response) {
+      return response.json();
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        updateTable(data);
-      })
-      .catch(error => console.error(error));
-}
-  
-function updateTable(data) {
-    // Get the table body element
-    const tableBody = document.querySelector('#table-body');
-  
-    // Clear the existing table rows
-    tableBody.innerHTML = '';
-     
-    // Loop through the array of objects and create table rows and cells
-    data.forEach(str => {
-        const row = document.createElement('tr');
-        const cell = document.createElement('td');
-        cell.textContent = str;
-        row.appendChild(cell);
-        tableBody.appendChild(row);
+    .then(function(data) {
+      if (data === true) {
+        document.querySelector('#categoriesTable th:last-child').style.display = 'table-cell';
+        document.querySelectorAll('.remove-category-button').forEach(function(button) {
+          button.style.display = 'inline-block';
+        });
+        document.querySelector('#categoriesTable tr:last-child').style.display = 'table-row';
+        document.querySelector('#addCategoryButton').addEventListener('click', function() {
+          var name = document.querySelector('#newCategoryName').value;
+          var weight = document.querySelector('#newCategoryWeight').value;
+          if (name && weight) {
+            // TODO: Implement add category functionality
+          }
+        });
+        document.querySelectorAll('.remove-category-button').forEach(function(button) {
+          button.addEventListener('click', function() {
+            var categoryId = button.getAttribute('data-category-id');
+            // TODO: Implement remove category functionality
+          });
+        });
+      }
+    });
+});
+
+function updateTables() {
+  fetch('/api/get-players')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var table = document.querySelector('#playersTable');
+      table.innerHTML = '';
+      var headerRow = document.createElement('tr');
+      var nameHeader = document.createElement('th');
+      nameHeader.textContent = 'Name';
+      headerRow.appendChild(nameHeader);
+      var scoreHeader = document.createElement('th');
+      scoreHeader.textContent = 'Score';
+      headerRow.appendChild(scoreHeader);
+      table.appendChild(headerRow);
+      data.forEach(function(player) {
+        var row = document.createElement('tr');
+        var nameCell = document.createElement('td');
+        nameCell.textContent = player;
+        row.appendChild(nameCell);
+        var scoreCell = document.createElement('td');
+        scoreCell.textContent = '0';
+        row.appendChild(scoreCell);
+        table.appendChild(row);
       });
+    });
+  fetch('/api/get-categories')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var table = document.querySelector('#categoriesTable');
+      table.innerHTML = '';
+      var headerRow = document.createElement('tr');
+      var nameHeader = document.createElement('th');
+      nameHeader.textContent = 'Name';
+      headerRow.appendChild(nameHeader);
+      var weightHeader = document.createElement('th');
+      weightHeader.textContent = 'Weight';
+      headerRow.appendChild(weightHeader);
+      var removeHeader = document.createElement('th');
+      removeHeader.textContent = 'Remove';
+      removeHeader.style.display = 'none';
+      headerRow.appendChild(removeHeader);
+      table.appendChild(headerRow);
+      data.forEach(function(category) {
+        var row = document.createElement('tr');
+        var nameCell = document.createElement('td');
+        nameCell.textContent = category;
+        row.appendChild(nameCell);
+        var weightCell = document.createElement('td');
+        weightCell.textContent = '1';
+        row.appendChild(weightCell);
+        var removeCell = document.createElement('td');
+        var removeButton = document.createElement('button');
+        removeButton.setAttribute('type', 'button');
+        removeButton.classList.add('remove-category-button');
+        removeButton.setAttribute('data-category-id', category);
+        removeButton.textContent = 'Remove';
+        removeButton.style.display = 'none';
+        removeCell.appendChild(removeButton);
+        row.appendChild(removeCell);
+        table.appendChild(row);
+      });
+    });
 }
 
 const disconnect = () => {
@@ -47,7 +111,8 @@ const disconnect = () => {
       method: 'POST',
       headers: {
           'Authorization': jwtToken,
-          'Content-Type': 'application/json'
+          'HostUsername': username,
+          'GameID': gameId
       }
   })
   .then(response => response.json)
@@ -55,7 +120,7 @@ const disconnect = () => {
     console.log(data);
     fetchData();
     setTimeout(function() {
-      window.location.href = "http://192.168.1.27:8081/auth/authenticate";
+      window.location.href = "http://192.168.1.27:8081/home";
   }, 2000);
   })
   .catch(error => console.error(error));
