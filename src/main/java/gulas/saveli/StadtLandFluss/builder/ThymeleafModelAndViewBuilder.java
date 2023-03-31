a20a7af56498642480aa57bfd14228f010244093
saveligulas <saveli.gulas@gmail.com>
2023-03-31 22:48:52 +0200

    removed config and added service annotation

--- src/main/java/gulas/saveli/StadtLandFluss/builder/ThymeleafModelAndViewBuilder.java
+++ src/main/java/gulas/saveli/StadtLandFluss/builder/ThymeleafModelAndViewBuilder.java
@@ -1,7 +1,9 @@
 package gulas.saveli.StadtLandFluss.builder;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
+@Service
 public class ThymeleafModelAndViewBuilder {
 
     public ModelAndView build(String path) {
