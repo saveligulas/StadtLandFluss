c8ca255326f08b242cc759c7ceb78e11f361f20b
saveligulas <saveli.gulas@gmail.com>
2023-04-05 13:24:49 +0200

    changed controller from rest controller to normal controller and added mapping to get ModelAndView of thymeleaf

--- src/main/java/gulas/saveli/StadtLandFluss/game/controller/GameController.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/controller/GameController.java
@@ -1,25 +1,28 @@
 package gulas.saveli.StadtLandFluss.game.controller;
 
 import gulas.saveli.StadtLandFluss.builder.ThymeleafModelAndViewBuilder;
+import gulas.saveli.StadtLandFluss.game.models.resp.GameViewResponse;
+import gulas.saveli.StadtLandFluss.game.service.GameService;
+import gulas.saveli.StadtLandFluss.game.service.InGameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/game")
 @RequiredArgsConstructor
 public class GameController {
 
     @Autowired
-    private final ThymeleafModelAndViewBuilder thymeleafModelAndViewBuilder;
+    private final InGameService inGameService;
 
-
-    @GetMapping
-    public ModelAndView viewGame() {
-        return thymeleafModelAndViewBuilder.build("game");
+    @GetMapping("/{gameId}")
+    public ModelAndView getGameById(@PathVariable("gameId") String gameId) {
+        GameViewResponse response = inGameService.getGameViewResponse(Long.parseLong(gameId));
     }
 
 }
