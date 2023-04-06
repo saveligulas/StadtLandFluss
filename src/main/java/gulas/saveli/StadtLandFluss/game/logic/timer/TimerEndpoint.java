550ccadb485b069f4c7c52342b3c3fa750e4df50
saveligulas <saveli.gulas@gmail.com>
2023-04-06 14:00:22 +0200

    added onopen method

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
@@ -1,9 +1,13 @@
 package gulas.saveli.StadtLandFluss.game.logic.timer;
 
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
 import java.util.Map;
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint("/timer/{gameId}")
@@ -11,5 +15,21 @@ public class TimerEndpoint {
 
     private static final Map<String, Timer> gameTimers = new ConcurrentHashMap<>();
 
-
+    @OnOpen
+    public void onOpen(Session session, @PathParam("gameId") String gameId) {
+        gameTimers.computeIfAbsent(gameId, k -> {
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    for (Session session : session.getOpenSessions()) {
+                        if (session.getUserPrincipal() != null && gameId.equals(session.getUserPrincipal().getName())) {
+                            session.getAsyncRemote().sendText("timer_event");
+                        }
+                    }
+                }
+            }, 0, 1000);
+            return timer;
+        });
+    }
 }
