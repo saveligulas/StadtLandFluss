2c855d412231ab7a962113e02fdc73f6a4454df4
saveligulas <saveli.gulas@gmail.com>
2023-04-06 14:11:34 +0200

    added onclose and changed onopen

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
@@ -1,35 +1,46 @@
 package gulas.saveli.StadtLandFluss.game.logic.timer;
 
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint("/timer/{gameId}")
 public class TimerEndpoint {
 
-    private static final Map<String, Timer> gameTimers = new ConcurrentHashMap<>();
+    private static final Map<String, List<Session>> sessionsMap = new HashMap<>();
+    private static final Map<String, Timer> timersMap = new HashMap<>();
 
     @OnOpen
     public void onOpen(Session session, @PathParam("gameId") String gameId) {
-        gameTimers.computeIfAbsent(gameId, k -> {
-            Timer timer = new Timer();
-            timer.scheduleAtFixedRate(new TimerTask() {
-                @Override
-                public void run() {
-                    for (Session session : session.getOpenSessions()) {
-                        if (session.getUserPrincipal() != null && gameId.equals(session.getUserPrincipal().getName())) {
-                            session.getAsyncRemote().sendText("timer_event");
-                        }
-                    }
-                }
-            }, 0, 1000);
-            return timer;
-        });
+        // Add the session to the list of sessions for the game
+        List<Session> gameSessions = sessionsMap.getOrDefault(gameId, new ArrayList<>());
+        gameSessions.add(session);
+        sessionsMap.put(gameId, gameSessions);
+        session.getUserProperties().put("gameId", gameId);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        // Remove the session from the list of sessions for the game
+        String gameId = (String) session.getUserProperties().get("gameId");
+        List<Session> gameSessions = sessionsMap.get(gameId);
+        gameSessions.remove(session);
+        session.getUserProperties().remove("gameId");
+
+        // If there are no more sessions for the game, cancel the timer
+        if (gameSessions.isEmpty()) {
+            Timer timer = timersMap.get(gameId);
+            if (timer != null) {
+                timer.cancel();
+                timer.purge();
+                timersMap.remove(gameId);
+            }
+        }
     }
 }
