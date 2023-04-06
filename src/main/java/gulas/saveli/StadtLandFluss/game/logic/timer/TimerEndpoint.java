e78ed6ea21b3941bce097bb7bbfc529bc9e691ff
saveligulas <saveli.gulas@gmail.com>
2023-04-06 14:13:21 +0200

    added onmessage and timerupdate

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
@@ -7,8 +7,8 @@ import jakarta.websocket.Session;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
+import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint("/timer/{gameId}")
 public class TimerEndpoint {
@@ -18,7 +18,6 @@ public class TimerEndpoint {
 
     @OnOpen
     public void onOpen(Session session, @PathParam("gameId") String gameId) {
-        // Add the session to the list of sessions for the game
         List<Session> gameSessions = sessionsMap.getOrDefault(gameId, new ArrayList<>());
         gameSessions.add(session);
         sessionsMap.put(gameId, gameSessions);
@@ -27,19 +26,62 @@ public class TimerEndpoint {
 
     @OnClose
     public void onClose(Session session) {
-        // Remove the session from the list of sessions for the game
         String gameId = (String) session.getUserProperties().get("gameId");
         List<Session> gameSessions = sessionsMap.get(gameId);
         gameSessions.remove(session);
         session.getUserProperties().remove("gameId");
 
-        // If there are no more sessions for the game, cancel the timer
         if (gameSessions.isEmpty()) {
             Timer timer = timersMap.get(gameId);
             if (timer != null) {
                 timer.cancel();
                 timer.purge();
                 timersMap.remove(gameId);
+            }
+        }
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        if (message.equals("join")) {
+            String gameId = (String) session.getUserProperties().get("gameId");
+            List<Session> gameSessions = sessionsMap.get(gameId);
+            gameSessions.add(session);
+            session.getUserProperties().put("playerIndex", gameSessions.size() - 1);
+
+            if (gameSessions.size() == 1) {
+                Timer timer = new Timer();
+                TimerTask task = new TimerTask() {
+                    int timeLeft = 10 * 60; // 10 minutes
+
+                    @Override
+                    public void run() {
+                        timeLeft--;
+                        if (timeLeft == 0) {
+                            timer.cancel();
+                            timer.purge();
+                            sendTimerUpdate(gameId, "00:00");
+                        } else {
+                            int minutes = timeLeft / 60;
+                            int seconds = timeLeft % 60;
+                            String timerValue = String.format("%02d:%02d", minutes, seconds);
+                            sendTimerUpdate(gameId, timerValue);
+                        }
+                    }
+                };
+                timer.scheduleAtFixedRate(task, 0, 1000);
+                timersMap.put(gameId, timer);
+            }
+        }
+    }
+
+    private void sendTimerUpdate(String gameId, String timerValue) {
+        List<Session> gameSessions = sessionsMap.get(gameId);
+        for (Session session : gameSessions) {
+            try {
+                session.getBasicRemote().sendText(timerValue);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
