d1fe3f4eff7bddc9bde3b55ae8fd87dac996748f
saveligulas <saveli.gulas@gmail.com>
2023-04-11 12:48:04 +0200

    implemented new WebsocketConfigurer

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -1,13 +1,26 @@
 package gulas.saveli.StadtLandFluss.security.config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 // @Configuration
-public class WebSocketSecurityConfig {
+public class WebSocketSecurityConfig  implements WebSocketConfigurer, WebSecurityConfigurer<WebSecurity> {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 
-//    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-//        messages
-//                .simpDestMatchers("/timer/**").permitAll();
-//    }
+    }
+
+    @Override
+    public void init(WebSecurity builder) throws Exception {
+
+    }
+
+    @Override
+    public void configure(WebSecurity builder) throws Exception {
+
+    }
 }
