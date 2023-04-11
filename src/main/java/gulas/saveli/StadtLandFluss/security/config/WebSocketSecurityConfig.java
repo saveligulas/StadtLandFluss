5d742c4377efdf10ff5e13f78d1de42ca41afe69
saveligulas <saveli.gulas@gmail.com>
2023-04-11 12:18:20 +0200

    added basic config

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -1,4 +1,20 @@
 package gulas.saveli.StadtLandFluss.security.config;
+import gulas.saveli.StadtLandFluss.user.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 
+@Configuration
+@EnableWebSocketSecurity
 public class WebSocketSecurityConfig {
+
+    @Bean
+    AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+        messages
+                .simpDestMatchers("/user/**").authenticated()
+
+        return messages.build();
+    }
 }
