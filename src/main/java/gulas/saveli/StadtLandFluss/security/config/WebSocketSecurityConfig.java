927759e403f366b3c4b075791823415c228dbae2
saveligulas <saveli.gulas@gmail.com>
2023-04-11 12:20:14 +0200

    cleanup

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -1,12 +1,7 @@
 package gulas.saveli.StadtLandFluss.security.config;
-import gulas.saveli.StadtLandFluss.user.User;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 
 @Configuration
 public class WebSocketSecurityConfig
