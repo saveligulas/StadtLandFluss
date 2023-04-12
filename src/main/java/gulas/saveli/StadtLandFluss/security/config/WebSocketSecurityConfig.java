0c9a37565cd45bec68ac099266046cfef1ced08c
saveligulas <saveli.gulas@gmail.com>
2023-04-12 11:15:05 +0200

    removed imports

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -1,9 +1,6 @@
 package gulas.saveli.StadtLandFluss.security.config;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
