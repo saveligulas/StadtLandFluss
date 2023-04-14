ba71ae0e199886cc12108b370335b97f5688155e
saveligulas <saveli.gulas@gmail.com>
2023-04-14 12:43:16 +0200

    removed unnecessary code

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -2,14 +2,16 @@ package gulas.saveli.StadtLandFluss.security.config;
 import gulas.saveli.StadtLandFluss.game.logic.timer.TimerEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-public class WebSocketSecurityConfig  implements WebSocketConfigurer, WebSecurityConfigurer<WebSecurity> {
+public class WebSocketSecurityConfig  implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler((WebSocketHandler) timerEndpoint(), "/timer/{gameId}");
@@ -18,15 +20,5 @@ public class WebSocketSecurityConfig  implements WebSocketConfigurer, WebSecurit
     @Bean
     public TimerEndpoint timerEndpoint() {
         return new TimerEndpoint();
-    }
-
-    @Override
-    public void init(WebSecurity web) throws Exception {
-        web.addSecurityFilterChainBuilder()
-    }
-
-    @Override
-    public void configure(WebSecurity builder) throws Exception {
-
     }
 }
