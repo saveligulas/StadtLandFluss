41cd7a43901df079b21b1c7cef85afb919eee5d3
saveligulas <saveli.gulas@gmail.com>
2023-04-15 19:27:46 +0200

    removed unnecessary import

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/TimerEndpoint.java
@@ -3,7 +3,6 @@ package gulas.saveli.StadtLandFluss.game.logic.timer;
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
-import jakarta.websocket.Session;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.web.socket.TextMessage;
