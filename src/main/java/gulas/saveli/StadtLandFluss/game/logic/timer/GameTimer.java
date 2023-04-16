53417f3fd4c4a9818079ed818cdc47c178e85cf7
saveligulas <saveli.gulas@gmail.com>
2023-04-16 22:00:45 +0200

    implementing websockethandler

--- src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/GameTimer.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/logic/timer/GameTimer.java
@@ -1,9 +1,7 @@
 package gulas.saveli.StadtLandFluss.game.logic.timer;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
@@ -13,54 +11,31 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 @Component
-public class GameTimer extends TextWebSocketHandler {
+public class GameTimer implements WebSocketHandler {
 
-    private final Map<String, Timer> timers = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        String gameId = session.getUri().getPath().split("/")[2];
-        System.out.println("New connection for gameId: " + gameId);
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-            int timeLeft = 5 * 60; // 5 minutes
 
-            @Override
-            public void run() {
-                timeLeft--;
-                if (timeLeft == 0) {
-                    timer.cancel();
-                    timer.purge();
-                    try {
-                        session.close(CloseStatus.NORMAL.withReason("Game over!"));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    int minutes = timeLeft / 60;
-                    int seconds = timeLeft % 60;
-                    String timerValue = String.format("%02d:%02d", minutes, seconds);
-                    try {
-                        session.sendMessage(new TextMessage(timerValue));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        timer.scheduleAtFixedRate(task, 0, 1000);
-        timers.put(gameId, timer);
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        String gameId = session.getUri().getPath().split("/")[2];
-        System.out.println("Connection closed for gameId: " + gameId);
-        Timer timer = timers.remove(gameId);
-        if (timer != null) {
-            timer.cancel();
-            timer.purge();
-        }
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+
     }
 
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
 }
