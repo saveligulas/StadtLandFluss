b654d53782395b66fe9ac0eeb8750c70b6e95e42
saveligulas <saveli.gulas@gmail.com>
2023-04-17 20:53:24 +0200

    updated config

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -5,18 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
+@EnableWebSocket
 @RequiredArgsConstructor
 public class WebSocketSecurityConfig implements WebSocketConfigurer {
+    @Autowired
+    private final GameTimer countdownWebSocketHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler((WebSocketHandler) timerEndpoint, "/timer/{gameId}")
+        registry.addHandler(countdownWebSocketHandler, "/timer")
                 .setAllowedOrigins("*");
     }
-
-    @Autowired
-    private final GameTimer timerEndpoint;
 }
