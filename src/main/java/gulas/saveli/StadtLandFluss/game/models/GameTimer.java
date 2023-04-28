1175307081251f85524a81328de001b0c953e8a8
saveligulas <saveli.gulas@gmail.com>
2023-04-28 11:23:45 +0200

    new data

--- src/main/java/gulas/saveli/StadtLandFluss/game/models/GameTimer.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/models/GameTimer.java
@@ -1,14 +1,23 @@
 package gulas.saveli.StadtLandFluss.game.models;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 @Data
+@Builder
 public class GameTimer {
 
     private Timer timer;
     private Boolean isRunning;
     private Long gameId;
+    private int roundCount;
 
+    public void purgeTimer() {
+        this.timer.cancel();
+        this.timer = new Timer();
+        this.roundCount += 1;
+    }
 }
