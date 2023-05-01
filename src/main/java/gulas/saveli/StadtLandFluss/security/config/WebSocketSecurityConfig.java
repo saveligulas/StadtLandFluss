e852cf68e24b460ea8a5946b212bf8a0e87766dd
saveligulas <saveli.gulas@gmail.com>
2023-05-01 13:54:25 +0200

    implemented methods

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -4,10 +4,16 @@ import gulas.saveli.StadtLandFluss.game.websocket.GameTimerLogic;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocket
@@ -27,5 +33,18 @@ public class WebSocketSecurityConfig implements WebSocketConfigurer {
     }
 
     private class MyHandshakeInterceptor implements HandshakeInterceptor {
+        @Override
+        public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+            List<String> username = request.getHeaders().get("Username");
+            if(!username.isEmpty()) {
+                attributes.put("Username", username.get(0));
+            }
+            return true;
+        }
+
+        @Override
+        public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+        }
     }
 }
