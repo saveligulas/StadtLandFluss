e2b978b03629fc050dc8547e662b335768dacd58
saveligulas <saveli.gulas@gmail.com>
2023-05-01 14:22:42 +0200

    moved private class to different package

--- src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/config/WebSocketSecurityConfig.java
@@ -1,6 +1,7 @@
 package gulas.saveli.StadtLandFluss.security.config;
 import gulas.saveli.StadtLandFluss.game.websocket.GameConnectionHandler;
 import gulas.saveli.StadtLandFluss.game.websocket.GameTimerLogic;
+import gulas.saveli.StadtLandFluss.security.auth.websocket.UsernameHandShakeInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -23,28 +24,16 @@ public class WebSocketSecurityConfig implements WebSocketConfigurer {
     private final GameTimerLogic countdownWebSocketHandler;
     @Autowired
     private final GameConnectionHandler gameConnectionWebSocketHandler;
+    @Autowired
+    private final UsernameHandShakeInterceptor usernameHandShakeInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(countdownWebSocketHandler, "/ws/timer/{gameId}")
+                .addInterceptors(usernameHandShakeInterceptor)
                 .setAllowedOrigins("*")
                 .addHandler(gameConnectionWebSocketHandler, "/ws/{gameId}")
+                .addInterceptors(usernameHandShakeInterceptor)
                 .setAllowedOrigins("*");
-    }
-
-    private class MyHandshakeInterceptor implements HandshakeInterceptor {
-        @Override
-        public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-            List<String> username = request.getHeaders().get("Username");
-            if(!username.isEmpty()) {
-                attributes.put("Username", username.get(0));
-            }
-            return true;
-        }
-
-        @Override
-        public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
-
-        }
     }
 }
