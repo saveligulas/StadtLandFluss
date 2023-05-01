72870c4fc8ce69cccc178b149b5729f9925558d8
saveligulas <saveli.gulas@gmail.com>
2023-05-01 10:33:11 +0200

    fixed and added method

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocket.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocket.java
@@ -7,17 +7,24 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class WebSocket {
     private final Map<Long, List<WebSocketSession>> webSocketSessionsMap = new HashMap<>();
 
-    private void sendMessage(int countdown, Long id) {
+    private void sendMessage(String message, Long id) {
         try {
-            for(WebSocketSession session : webSocketSessionMap.get(id)) {
-                session.sendMessage(new TextMessage(Integer.toString(countdown)));
+            for(WebSocketSession session : webSocketSessionsMap.get(id)) {
+                session.sendMessage(new TextMessage(message));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private Long getGameIdFromSession(WebSocketSession session) {
+        String url = Objects.requireNonNull(session.getUri()).toString();
+        String gameIdStr = url.substring(url.lastIndexOf('/') + 1);
+        return Long.parseLong(gameIdStr);
     }
 }
