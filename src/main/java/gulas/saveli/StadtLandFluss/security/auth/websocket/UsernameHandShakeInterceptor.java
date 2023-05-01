6044d3956e716dadd0e4d444fa498f772541d9c6
saveligulas <saveli.gulas@gmail.com>
2023-05-01 14:22:23 +0200

    new Interceptor

---
+++ src/main/java/gulas/saveli/StadtLandFluss/security/auth/websocket/UsernameHandShakeInterceptor.java
@@ -0,0 +1,26 @@
+package gulas.saveli.StadtLandFluss.security.auth.websocket;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class UsernameHandShakeInterceptor implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        List<String> username = request.getHeaders().get("Username");
+        if(!username.isEmpty()) {
+            attributes.put("Username", username.get(0));
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+    }
+}
