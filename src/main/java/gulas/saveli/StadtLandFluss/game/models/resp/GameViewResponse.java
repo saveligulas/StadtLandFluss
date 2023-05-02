2410de5c6118c48b85fcea73268053d7367eb6b0
saveligulas <saveli.gulas@gmail.com>
2023-05-02 15:06:03 +0200

    added params

--- src/main/java/gulas/saveli/StadtLandFluss/game/models/resp/GameViewResponse.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/models/resp/GameViewResponse.java
@@ -1,14 +1,18 @@
 package gulas.saveli.StadtLandFluss.game.models.resp;
 
+import gulas.saveli.StadtLandFluss.game.models.Category;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class GameViewResponse {
 
+    private Integer currentRound;
     private Integer rounds;
     private Character currentCharacter;
-    private List<CategoryResponse> categories;
+    private List<Category> categories;
 
 }
