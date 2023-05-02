f521d647e2fc94c0858c40c273c5040b4f66a277
saveligulas <saveli.gulas@gmail.com>
2023-05-02 15:06:27 +0200

    finished method

--- src/main/java/gulas/saveli/StadtLandFluss/game/service/InGameService.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/service/InGameService.java
@@ -1,5 +1,7 @@
 package gulas.saveli.StadtLandFluss.game.service;
 
+import gulas.saveli.StadtLandFluss.errorHandler.handler.ApiRequestException;
+import gulas.saveli.StadtLandFluss.game.models.Game;
 import gulas.saveli.StadtLandFluss.game.models.resp.GameViewResponse;
 import gulas.saveli.StadtLandFluss.repo.GameRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,14 @@ public class InGameService {
     @Autowired
     private final GameRepository gameRepository;
 
-    public GameViewResponse getGameViewResponse(long parseLong) {
-        return null;
+    public GameViewResponse getGameViewResponse(Long gameId) {
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new ApiRequestException("game with id " + gameId + " does not exist"));
+        return GameViewResponse.builder()
+                .categories(game.getCategories())
+                .rounds(game.getRounds())
+                .currentRound(1) //TODO fix this
+                .currentCharacter(game.getCurrentCharacter())
+                .build();
     }
 }
