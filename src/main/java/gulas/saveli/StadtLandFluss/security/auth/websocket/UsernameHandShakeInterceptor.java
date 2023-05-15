85cb2d6d7e2f59e0943970c56da8f4264757cf57
saveligulas <saveli.gulas@gmail.com>
2023-05-15 21:52:59 +0200

    fixed method

--- src/main/java/gulas/saveli/StadtLandFluss/security/auth/websocket/UsernameHandShakeInterceptor.java
+++ src/main/java/gulas/saveli/StadtLandFluss/security/auth/websocket/UsernameHandShakeInterceptor.java
@@ -12,23 +12,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
 @Component
 public class UsernameHandShakeInterceptor implements HandshakeInterceptor {
+//    @Override
+//    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+//        HttpHeaders headers = request.getHeaders();
+//        List<String> protocols = headers.get("Sec-WebSocket-Protocol");
+//        if (protocols != null && !protocols.isEmpty()) {
+//            String protocol = protocols.get(0);
+//            System.out.println(protocol);
+//            attributes.put("Username", protocol);
+//        } else {
+//            throw new ApiRequestException("Invalid protocol used");
+//        }
+//        return true;
+//    }
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-        HttpHeaders headers = request.getHeaders();
-        List<String> protocols = headers.get("Sec-WebSocket-Protocol");
-        if (protocols != null && !protocols.isEmpty()) {
-            String protocol = protocols.get(0);
-            System.out.println(protocol);
-            attributes.put("Username", protocol);
-        } else {
-            throw new ApiRequestException("Invalid protocol used");
+        URI uri = request.getURI();
+        String query = uri.getQuery();
+        String username = extractUsernameFromQuery(query);
+        System.out.println(username);
+        if(username.isEmpty()) {
+            throw new ApiRequestException("Invalid protocol");
         }
+        attributes.put("Username", username);
         return true;
+    }
+
+    private String extractUsernameFromQuery(String query) {
+        String[] queryParams = query.split("&");
+        for (String param : queryParams) {
+            String[] keyValue = param.split("=");
+            if (keyValue.length == 2 && "username".equals(keyValue[0])) {
+                return URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+            }
+        }
+        return null;
     }
 
     @Override
