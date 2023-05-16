0a060cedb64f4cdb4790889f29214c0e56a9468a
saveligulas <saveli.gulas@gmail.com>
2023-05-16 19:46:12 +0200

    added new message

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameConnectionHandler.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameConnectionHandler.java
@@ -32,6 +32,7 @@ public class GameConnectionHandler extends WebSocket implements WebSocketHandler
             throw new ApiRequestException("game has started, expired or is full");
         }
         sendMessageToAll(getUsername(session)+gameId+webSocketSessionsMap.toString(), gameId);
+        sendMessageToAll(String.valueOf(webSocketSessionsMap.get(gameId).size()) + ": Session count", gameId);
     }
 
     @Override
