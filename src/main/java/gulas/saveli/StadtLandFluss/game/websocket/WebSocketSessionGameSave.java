b06dfc2797afa9981f0d7aee055f155d13fed923
saveligulas <saveli.gulas@gmail.com>
2023-05-20 21:05:43 +0200

    added fields

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocketSessionGameSave.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocketSessionGameSave.java
@@ -4,8 +4,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.Timer;
+
 @Data
 @Builder
 @RequiredArgsConstructor
 public class WebSocketSessionGameSave {
+    private Long id;
+    private List<WebSocketSessionPlayerSave> playerSaves;
+    private Timer timer;
+    private GameState gameState;
 }
