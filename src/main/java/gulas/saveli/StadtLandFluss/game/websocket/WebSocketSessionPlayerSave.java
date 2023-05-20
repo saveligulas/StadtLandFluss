a21a3c21416c4d2a604a6fb38265b3d07313b379
saveligulas <saveli.gulas@gmail.com>
2023-05-20 21:01:57 +0200

    added field

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocketSessionPlayerSave.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocketSessionPlayerSave.java
@@ -3,9 +3,12 @@ package gulas.saveli.StadtLandFluss.game.websocket;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.socket.WebSocketSession;
 
 @Data
 @Builder
 @RequiredArgsConstructor
 public class WebSocketSessionPlayerSave {
+    private WebSocketSession session;
+
 }
