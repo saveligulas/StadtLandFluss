d49d889dc23231dd27ee58be4d4378636d3f5eaf
saveligulas <saveli.gulas@gmail.com>
2023-05-20 21:04:06 +0200

    added annotations

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocketSessionGameSave.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/WebSocketSessionGameSave.java
@@ -1,7 +1,11 @@
 package gulas.saveli.StadtLandFluss.game.websocket;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@Builder
+@RequiredArgsConstructor
 public class WebSocketSessionGameSave {
 }
