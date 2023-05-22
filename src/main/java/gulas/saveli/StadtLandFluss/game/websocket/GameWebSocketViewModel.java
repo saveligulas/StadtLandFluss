084d023d3eb2a031fd0292a813ad80bcf80f600e
saveligulas <saveli.gulas@gmail.com>
2023-05-22 21:48:02 +0200

    added method

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketViewModel.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketViewModel.java
@@ -1,10 +1,15 @@
 package gulas.saveli.StadtLandFluss.game.websocket;
 
 import gulas.saveli.StadtLandFluss.game.websocket.models.WebSocketSessionGameSave;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameWebSocketViewModel {
     private final List<WebSocketSessionGameSave> webSocketSessionGameSaves = new ArrayList<>();
+
+    public void saveWebSocketSession(Long id, WebSocketSession webSocketSession) {
+
+    }
 }
