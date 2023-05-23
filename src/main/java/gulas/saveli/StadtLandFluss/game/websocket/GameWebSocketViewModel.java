7fdf58a980f49fc952e99eb200f1344e6895ad1e
saveligulas <saveli.gulas@gmail.com>
2023-05-23 17:34:44 +0200

    added builder

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketViewModel.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketViewModel.java
@@ -1,5 +1,6 @@
 package gulas.saveli.StadtLandFluss.game.websocket;
 
+import gulas.saveli.StadtLandFluss.game.websocket.models.GameState;
 import gulas.saveli.StadtLandFluss.game.websocket.models.WebSocketSessionGameSave;
 import gulas.saveli.StadtLandFluss.game.websocket.models.WebSocketSessionPlayerSave;
 import org.springframework.web.socket.WebSocketSession;
@@ -22,6 +23,13 @@ public class GameWebSocketViewModel {
                 foundGameSave = true;
                 webSocketSessionGameSaves.get(i).addPlayerSave(playerSave);
             }
+        }
+        if(!foundGameSave) {
+            webSocketSessionGameSaves.add(WebSocketSessionGameSave.builder()
+                            .gameState(GameState.PRE)
+                            .id(id)
+                            .playerSaves(List.of(playerSave))
+                    .build());
         }
     }
 }
