c01fceab707b33fac80f2fbe5cc87932df82a025
saveligulas <saveli.gulas@gmail.com>
2023-05-23 17:23:20 +0200

    added conversion

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketViewModel.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketViewModel.java
@@ -1,6 +1,7 @@
 package gulas.saveli.StadtLandFluss.game.websocket;
 
 import gulas.saveli.StadtLandFluss.game.websocket.models.WebSocketSessionGameSave;
+import gulas.saveli.StadtLandFluss.game.websocket.models.WebSocketSessionPlayerSave;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.ArrayList;
@@ -9,7 +10,10 @@ import java.util.List;
 public class GameWebSocketViewModel {
     private final List<WebSocketSessionGameSave> webSocketSessionGameSaves = new ArrayList<>();
 
-    public void saveWebSocketSession(Long id, WebSocketSession webSocketSession) {
-
+    public void saveWebSocketSession(Long id, String username, WebSocketSession webSocketSession) {
+        WebSocketSessionPlayerSave playerSave = WebSocketSessionPlayerSave.builder()
+                .session(webSocketSession)
+                .username(username)
+                .build();
     }
 }
