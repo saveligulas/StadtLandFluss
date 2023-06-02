e28882045a718d9bc3de3e6c0dcd34abe23a54d3
saveligulas <saveli.gulas@gmail.com>
2023-06-02 21:00:16 +0200

    fixed mapping

--- src/main/java/gulas/saveli/StadtLandFluss/game/controller/TimerTestController.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/controller/TimerTestController.java
@@ -2,12 +2,14 @@ package gulas.saveli.StadtLandFluss.game.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/test")
 public class TimerTestController {
 
-    @GetMapping("/test/timer")
+    @GetMapping("/timer")
     public ModelAndView timer() {
         return  new ModelAndView("timer");
     }
