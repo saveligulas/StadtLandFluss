a356d23ff17fcb7affa3163687f66eeb4d4193b4
saveligulas <saveli.gulas@gmail.com>
2023-06-03 21:13:37 +0200

    added inheritance and interface

--- src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketHandler.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/websocket/GameWebSocketHandler.java
@@ -1,4 +1,34 @@
 package gulas.saveli.StadtLandFluss.game.websocket;
 
-public class GameWebSocketHandler {
+import gulas.saveli.StadtLandFluss.game.websocket.models.GameWebSocketViewModel;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+public class GameWebSocketHandler extends GameWebSocketViewModel implements WebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
 }
