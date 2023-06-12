eec3f71db6bf7232599ee8eaf4c1da78415b1766
saveligulas <saveli.gulas@gmail.com>
2023-06-12 15:58:25 +0200

    added query to find gameId with playerId

--- src/main/java/gulas/saveli/StadtLandFluss/repo/GameRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/GameRepository.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,6 +14,9 @@ import java.util.Optional;
 public interface GameRepository extends JpaRepository<Game, Long> {
     @Query("SELECT g FROM Game g WHERE g.hostUsername = ?1")
     Optional<Game> findByHostUsername(String username);
+
+    @Query("SELECT g.id FROM Game g JOIN g.players p WHERE p.id = :playersId")
+    Long findGameIdByPlayerId(@Param("playersId") Long playersId);
 
     @Modifying
     @Transactional
