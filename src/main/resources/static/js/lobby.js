774f0d99813dda74fa4f57dcb926c7b90f278433
saveligulas <saveli.gulas@gmail.com>
2023-06-13 22:35:12 +0200

    added address

--- src/main/resources/static/js/lobby.js
+++ src/main/resources/static/js/lobby.js
@@ -203,7 +203,13 @@ const disconnect = () => {
   .catch(error => console.error(error));
 };
 
+startButton.addEventListener('click', startGame);
 
+function startGame() {
+  fetch(`http://192.168.1.27:8081/lobby/${gameId}/host/start`) {
+    method
+  }
+}
 
 const addEventListener = () => {
   disconnectButton.addEventListener('click', disconnect);
