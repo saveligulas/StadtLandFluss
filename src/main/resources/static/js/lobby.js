bc3b17b1fba98908d0d3de2acff1edfc1f964d9a
saveligulas <saveli.gulas@gmail.com>
2023-06-15 15:49:27 +0200

    added check to see if response is ok

--- src/main/resources/static/js/lobby.js
+++ src/main/resources/static/js/lobby.js
@@ -213,6 +213,13 @@ function startGame() {
       'Username': username
     }
   })
+  .then(response => {
+    if(response.ok) {
+      return response.text();
+    } else {
+
+    }
+  })
   
 };
 
