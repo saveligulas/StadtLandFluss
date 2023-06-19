9c8cf155582dd83e5884e66b8dfd0ea911d7cc91
saveligulas <saveli.gulas@gmail.com>
2023-06-19 15:05:01 +0200

    added function

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -3,4 +3,8 @@ import { extractGameIdFromUrlAtEnd } from './cookies.js';
 console.log("loaded");
 const jwtToken = getCookie('Authorization');
 const username = getCookie('Username');
-const gameId = extractGameIdFromUrlAtEnd(window.location.href);
+const gameId = extractGameIdFromUrlAtEnd(window.location.href); 
+
+function checkIfGameHasExpired() {
+    fetch(`http://192.168.1.27:8081/game/${gameId}/`)
+}
