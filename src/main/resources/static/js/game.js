90d8c1fa73251f83afe1b2e6b9bc04f7dcd2b378
saveligulas <saveli.gulas@gmail.com>
2023-06-21 14:14:07 +0200

    added headers

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -6,5 +6,11 @@ const username = getCookie('Username');
 const gameId = extractGameIdFromUrlAtEnd(window.location.href); 
 
 function checkIfGameHasExpired() {
-    fetch(`http://192.168.1.27:8081/game/${gameId}/`)
+    fetch(`http://192.168.1.27:8081/game/${gameId}/`, {
+        method: 'GET',
+        headers: {
+            'Authorization': jwtToken,
+            'Username': username
+        }
+    })
 }
