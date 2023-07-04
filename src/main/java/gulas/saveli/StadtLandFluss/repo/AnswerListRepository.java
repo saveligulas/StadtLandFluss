125a0db6f6c4f987a12a4ae914115d9f146731f0
saveligulas <saveli.gulas@gmail.com>
2023-07-04 16:32:58 +0200

    added search query for answerLists

--- src/main/java/gulas/saveli/StadtLandFluss/repo/AnswerListRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/AnswerListRepository.java
@@ -1,9 +1,16 @@
 package gulas.saveli.StadtLandFluss.repo;
 
 import gulas.saveli.StadtLandFluss.game.models.AnswerList;
+import gulas.saveli.StadtLandFluss.game.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AnswerListRepository extends JpaRepository<AnswerList, Long> {
+
+    @Query("SELECT a FROM AnswerList a WHERE a.name = ?1")
+    public Optional<AnswerList> findByName(String name);
 }
