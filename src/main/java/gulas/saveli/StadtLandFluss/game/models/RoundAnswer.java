cb492f7b16b7e168c798571770f076e19e2888d6
saveligulas <saveli.gulas@gmail.com>
2023-07-04 16:15:22 +0200

    added method to add answers

--- src/main/java/gulas/saveli/StadtLandFluss/game/models/RoundAnswer.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/models/RoundAnswer.java
@@ -3,6 +3,7 @@ package gulas.saveli.StadtLandFluss.game.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
 import java.util.Map;
 
 @Embeddable
@@ -11,8 +12,9 @@ public class RoundAnswer {
 
     private Integer round;
     private Character character;
-    private Map<Category, String> categoryTextMap;
+    private List<String> answers;
 
-    public void addToCategoryTextMapping() {
+    public void addAnswer(String answer) {
+        this.answers.add(answer);
     }
 }
