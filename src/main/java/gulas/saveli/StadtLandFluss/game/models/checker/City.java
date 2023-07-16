8dcfeedf40ed20a6c74e85c73b65af1ba7eeb916
saveligulas <saveli.gulas@gmail.com>
2023-07-16 21:09:06 +0200

    added basic entity annotations

--- src/main/java/gulas/saveli/StadtLandFluss/game/models/checker/City.java
+++ src/main/java/gulas/saveli/StadtLandFluss/game/models/checker/City.java
@@ -1,4 +1,12 @@
 package gulas.saveli.StadtLandFluss.game.models.checker;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class City {
 }
