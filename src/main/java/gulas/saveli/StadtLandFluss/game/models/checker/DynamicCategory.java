75f0146fa55297e1e469d2db05cc60f7f3bcd525
saveligulas <saveli.gulas@gmail.com>
2023-09-02 17:52:01 +0200

    moved model from new project

---
+++ src/main/java/gulas/saveli/StadtLandFluss/game/models/checker/DynamicCategory.java
@@ -0,0 +1,37 @@
+package gulas.saveli.StadtLandFluss.game.models.checker;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "dynamic_category", schema = "converter")
+public class DynamicCategory {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "customTable_id")
+    private CustomTable customTable;
+
+    @OneToMany(mappedBy = "dynamicCategory", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderColumn
+    @ToString.Exclude
+    private List<DynamicField> dynamicFields = new ArrayList<>();
+
+    public void addDynamicField(DynamicField field) {
+        this.dynamicFields.add(field);
+    }
+}
