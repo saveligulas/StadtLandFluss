f676382e461810d5a0ea422b360c2b0417f2454d
saveligulas <saveli.gulas@gmail.com>
2023-09-02 17:51:51 +0200

    moved model from new project

---
+++ src/main/java/gulas/saveli/StadtLandFluss/game/models/checker/CustomTable.java
@@ -0,0 +1,33 @@
+package gulas.saveli.StadtLandFluss.game.models.checker;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name= "custom_table", schema= "converter", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
+public class CustomTable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String extension;
+
+    @OneToMany(mappedBy = "customTable", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @OrderColumn
+    @ToString.Exclude
+    private List<DynamicCategory> dynamicCategories = new ArrayList<>();
+
+    public void addDynamicCategory(DynamicCategory category) {
+        this.dynamicCategories.add(category);
+    }
+}
