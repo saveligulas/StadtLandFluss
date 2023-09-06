984cebeaf1bbb33e067289af4701d864f38fa7f4
saveligulas <saveli.gulas@gmail.com>
2023-09-06 13:34:45 +0200

    moved repo to different package

--- src/main/java/gulas/saveli/StadtLandFluss/repo/imp/DistinctNameRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/imp/DistinctNameRepository.java
@@ -1,4 +1,4 @@
-package gulas.saveli.StadtLandFluss.repo;
+package gulas.saveli.StadtLandFluss.repo.imp;
 
 import gulas.saveli.CSV2PostgreSQL.model.DistinctName;
 import gulas.saveli.CSV2PostgreSQL.model.enums.ModelType;
