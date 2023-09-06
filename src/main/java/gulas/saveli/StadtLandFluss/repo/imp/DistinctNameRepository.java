bd609328d0f4c823e0720b5d4fea5aaeb9cd1706
saveligulas <saveli.gulas@gmail.com>
2023-09-06 13:35:33 +0200

    removed false imports

--- src/main/java/gulas/saveli/StadtLandFluss/repo/imp/DistinctNameRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/imp/DistinctNameRepository.java
@@ -1,8 +1,5 @@
 package gulas.saveli.StadtLandFluss.repo.imp;
 
-import gulas.saveli.CSV2PostgreSQL.model.DistinctName;
-import gulas.saveli.CSV2PostgreSQL.model.enums.ModelType;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
