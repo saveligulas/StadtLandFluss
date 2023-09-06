7490555410f576bdc57105a1ba39d47644b1f014
saveligulas <saveli.gulas@gmail.com>
2023-09-06 13:35:41 +0200

    removed false imports

--- src/main/java/gulas/saveli/StadtLandFluss/repo/imp/DynamicFieldRepository.java
+++ src/main/java/gulas/saveli/StadtLandFluss/repo/imp/DynamicFieldRepository.java
@@ -1,7 +1,5 @@
 package gulas.saveli.StadtLandFluss.repo.imp;
 
-import gulas.saveli.CSV2PostgreSQL.model.DynamicField;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
